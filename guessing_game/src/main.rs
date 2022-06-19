76f18af3c40d28f10a72e2b2ca11170c6ee7fcc1
yukyu30 <fukanoyugo@gmail.com>
2022-06-19 22:39:54 +0900

    update: 数当てゲームが完成

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,24 +7,32 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is:{}", secret_number); //秘密の数字は次の通り
+    //println!("The secret number is:{}", secret_number); //秘密の数字は次の通り
 
-    println!("Please input your guess."); //ほら、予想を入力してね
+    loop{
+        println!("Please input your guess."); //ほら、予想を入力してね
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Faild to read line");
+        let mut guess = String::new();
     
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type number");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Faild to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
+   
 }
