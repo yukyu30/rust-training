a4ac523188abf176afb8706db1a3a2cac28884dd
yukyu30 <fukanoyugo@gmail.com>
2022-06-19 22:26:32 +0900

    update: 乱数生成する機能、乱数と入力を比較する機能を追加

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!"); //数を当ててごらん
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is:{}", secret_number); //秘密の数字は次の通り
 
     println!("Please input your guess."); //ほら、予想を入力してね
 
@@ -10,6 +16,15 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Faild to read line");
+    
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type number");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
